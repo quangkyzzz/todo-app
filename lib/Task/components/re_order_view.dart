175398ad1ff0757864afc0492085fe2bb60d40c8
quangkyzzz <57524232+quangkyzzz@users.noreply.github.com>
2024-06-19 11:11:44 +0700

    complete reorder view

--- lib/Task/components/re_order_view.dart
+++ lib/Task/components/re_order_view.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:todo_app/Models/task_model.dart';
 import 'package:todo_app/Task/components/incomplete_list.dart';
+import 'package:todo_app/Task/components/task_list_item.dart';
 
 class ReOrderView extends StatefulWidget {
   const ReOrderView({super.key});
@@ -23,6 +25,12 @@ class _ReOrderViewState extends State<ReOrderView> {
       'isCompleted': false,
       'dueDate': DateTime.now(),
     },
+    {
+      'taskID': '3',
+      'title': 'task 3',
+      'isCompleted': false,
+      'dueDate': DateTime.now(),
+    },
   ];
   @override
   Widget build(BuildContext context) {
@@ -30,7 +38,30 @@ class _ReOrderViewState extends State<ReOrderView> {
       appBar: AppBar(
         title: const Text('Reorder tasks'),
       ),
-      body: IncompleteList(taskList: incompleteTask),
+      body: ReorderableListView(
+          header: const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              'Tap and hold to reorder task',
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+          ),
+          children: [
+            for (int index = 0; index < incompleteTask.length; index++)
+              TaskListItem(
+                key: Key('$index'),
+                task: TaskModel.fromMap(incompleteTask[index]),
+              )
+          ],
+          onReorder: (int oldIndex, int newIndex) {
+            setState(() {
+              if (oldIndex < newIndex) {
+                newIndex -= 1;
+              }
+              final item = incompleteTask.removeAt(oldIndex);
+              incompleteTask.insert(newIndex, item);
+            });
+          }),
     );
   }
 }
