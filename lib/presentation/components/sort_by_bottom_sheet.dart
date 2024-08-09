4d5217ae3c2aba733897a3a1986b5de0861a0db0
quangkyzzz <57524232+quangkyzzz@users.noreply.github.com>
2024-08-09 12:04:39 +0700

    update: 'sort by' function

---
+++ lib/presentation/components/sort_by_bottom_sheet.dart
@@ -0,0 +1,102 @@
+// ignore_for_file: sized_box_for_whitespace
+
+import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:todo_app/models/task_list_model.dart';
+import 'package:todo_app/presentation/items/popup_item.dart';
+import 'package:todo_app/provider/task_list_provider.dart';
+import 'package:todo_app/themes.dart';
+
+class SortByBottomSheet extends StatelessWidget {
+  final TaskListModel taskList;
+  const SortByBottomSheet({super.key, required this.taskList});
+
+  @override
+  Widget build(BuildContext context) {
+    TaskListProvider taskListProvider =
+        Provider.of<TaskListProvider>(context, listen: false);
+    return Padding(
+      padding: const EdgeInsets.all(16.0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Text(
+            'Sort by',
+            style: MyTheme.itemTextStyle,
+          ),
+          const SizedBox(height: 8),
+          InkWell(
+            onTap: () {
+              taskListProvider.sortTaskListBy(
+                taskListID: taskList.id,
+                sortType: 'important',
+                isAscending: false,
+              );
+              Navigator.pop(context);
+            },
+            child: const PopupItem(
+              text: 'Important',
+              icon: Icons.star_border_outlined,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              taskListProvider.sortTaskListBy(
+                taskListID: taskList.id,
+                sortType: 'due_date',
+                isAscending: true,
+              );
+              Navigator.pop(context);
+            },
+            child: const PopupItem(
+              text: 'Due date',
+              icon: Icons.calendar_today_outlined,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              taskListProvider.sortTaskListBy(
+                taskListID: taskList.id,
+                sortType: 'my_day',
+                isAscending: false,
+              );
+              Navigator.pop(context);
+            },
+            child: const PopupItem(
+              text: 'Added to My Day',
+              icon: Icons.wb_sunny_outlined,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              taskListProvider.sortTaskListBy(
+                taskListID: taskList.id,
+                sortType: 'alphabetically',
+                isAscending: true,
+              );
+              Navigator.pop(context);
+            },
+            child: const PopupItem(
+              text: 'Alphabetically',
+              icon: Icons.import_export_outlined,
+            ),
+          ),
+          InkWell(
+            onTap: () {
+              taskListProvider.sortTaskListBy(
+                taskListID: taskList.id,
+                sortType: 'create_date',
+                isAscending: true,
+              );
+              Navigator.pop(context);
+            },
+            child: const PopupItem(
+              text: 'Creation date',
+              icon: Icons.more_time_outlined,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
