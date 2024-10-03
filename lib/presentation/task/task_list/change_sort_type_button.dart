ed80dc071ded150312811373e92ff5d433db7763
quangkyzzz <57524232+quangkyzzz@users.noreply.github.com>
2024-10-03 16:07:02 +0700

    change update function and convert to stateless widget at changeSortTypeButton

--- lib/presentation/task/task_list/change_sort_type_button.dart
+++ lib/presentation/task/task_list/change_sort_type_button.dart
@@ -4,7 +4,7 @@ import '../../../models/task_list.dart';
 import '../../../provider/settings_provider.dart';
 import '../../../view_models/task_list_view_model.dart';
 
-class ChangeSortTypeButton extends StatefulWidget {
+class ChangeSortTypeButton extends StatelessWidget {
   final TaskList taskList;
   const ChangeSortTypeButton({
     super.key,
@@ -12,64 +12,59 @@ class ChangeSortTypeButton extends StatefulWidget {
   });
 
   @override
-  State<ChangeSortTypeButton> createState() => _ChangeSortTypeButtonState();
-}
-
-class _ChangeSortTypeButtonState extends State<ChangeSortTypeButton> {
-  late TaskListViewModel taskListViewModel;
-  late SettingsProvider settingsProvider;
-  @override
-  void initState() {
-    taskListViewModel = context.read<TaskListViewModel>();
-    settingsProvider = context.read<SettingsProvider>();
-    super.initState();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Row(
       children: [
         TextButton(
           onPressed: () {
-            taskListViewModel.sortTaskListBy(
-              sortType: widget.taskList.sortByType!['sortType'],
-              isAscending: !widget.taskList.sortByType!['asc'],
-            );
-            taskListViewModel.updateTaskListWith(
-              settings: context.read<SettingsProvider>().settings,
+            context.read<TaskListViewModel>().sortTaskListBy(
+                  sortType: taskList.sortByType!['sortType'],
+                  isAscending: !taskList.sortByType!['asc'],
+                );
+            TaskList updatedTaskList =
+                context.read<TaskListViewModel>().currentTaskList.copyWith(
               sortByType: {
-                'sortType': widget.taskList.sortByType!['sortType'],
-                'asc': !widget.taskList.sortByType!['asc'],
+                'sortType': taskList.sortByType!['sortType'],
+                'asc': !taskList.sortByType!['asc'],
               },
             );
+            context
+                .read<TaskListViewModel>()
+                .updateTaskList(updatedTaskList: updatedTaskList);
           },
           child: Row(
             children: [
               Text(
-                'Sort by ${widget.taskList.sortByType!['sortType']}',
-                style: TextStyle(color: widget.taskList.themeColor),
+                'Sort by ${taskList.sortByType!['sortType']}',
+                style: TextStyle(color: taskList.themeColor),
               ),
-              (widget.taskList.sortByType!['asc'])
-                  ? Icon(Icons.expand_more, color: widget.taskList.themeColor)
+              (taskList.sortByType!['asc'])
+                  ? Icon(Icons.expand_more, color: taskList.themeColor)
                   : Icon(Icons.keyboard_arrow_up_outlined,
-                      color: widget.taskList.themeColor),
+                      color: taskList.themeColor),
             ],
           ),
         ),
         const Spacer(),
         IconButton(
           onPressed: () {
-            taskListViewModel.sortTaskListBy(
-              sortType: 'create date',
-              isAscending:
-                  (settingsProvider.settings.isAddNewTaskOnTop) ? false : true,
-            );
-            taskListViewModel.updateTaskListWithNull(setSortByType: true);
+            context.read<TaskListViewModel>().sortTaskListBy(
+                  sortType: 'create date',
+                  isAscending: (context
+                          .read<SettingsProvider>()
+                          .settings
+                          .isAddNewTaskOnTop)
+                      ? false
+                      : true,
+                );
+            context
+                .read<TaskListViewModel>()
+                .updateTaskListWithNull(setSortByType: true);
           },
           icon: Icon(
             Icons.close_outlined,
             size: 16,
-            color: widget.taskList.themeColor,
+            color: taskList.themeColor,
           ),
         ),
       ],
