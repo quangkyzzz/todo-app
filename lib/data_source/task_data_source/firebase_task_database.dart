d528e5529131ee837673b4626e88c3a3bc33ded5
quangkyzzz <quangkyzz@gmail.com>
2024-11-01 16:49:16 +0700

    implement all firebaseTaskDatabase function

--- lib/data_source/task_data_source/firebase_task_database.dart
+++ lib/data_source/task_data_source/firebase_task_database.dart
@@ -18,91 +18,126 @@ class FirebaseTaskDatabase implements TaskDatabaseProvider {
   }
 
   @override
-  Future<Task?> getTaskByID(
-      {required String groupID,
-      required String taskListID,
-      required String taskID}) {
-    // TODO: implement getTaskByID
-    throw UnimplementedError();
+  Future<Task?> getTaskByID({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+  }) async {
+    Task? result;
+    DataSnapshot snapshot = await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID')
+        .get();
+    if (snapshot.exists) {
+      Map resultMap = snapshot.value as Map;
+      result = Task.fromMap(resultMap);
+    }
+    return result;
   }
 
   @override
-  void createNewTask(
-      {required String groupID,
-      required String taskListID,
-      required Task newTask}) {
-    // TODO: implement createNewTask
+  void createNewTask({
+    required String groupID,
+    required String taskListID,
+    required Task newTask,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id${newTask.id}')
+        .set(newTask.toMap());
   }
 
   @override
-  void deleteTask(
-      {required String groupID,
-      required String taskListID,
-      required String taskID}) {
-    // TODO: implement deleteTask
+  void deleteTask({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID')
+        .remove();
   }
 
   @override
-  void updateTaskTitle(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required String newTitle}) {
-    // TODO: implement updateTaskTitle
+  void updateTaskTitle({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required String newTitle,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID')
+        .update({'title': newTitle});
   }
 
   @override
-  void updateDueDate(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required DateTime? dueDate}) {
-    // TODO: implement updateDueDate
+  void updateDueDate({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required DateTime? dueDate,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID')
+        .update({'dueDate': dueDate?.toString()});
   }
 
   @override
-  void updateFrequencyMultiplier(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required int frequencyMultiplier}) {
-    // TODO: implement updateFrequencyMultiplier
+  void updateFrequencyMultiplier({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required int frequencyMultiplier,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID')
+        .update({'frequencyMultiplier': frequencyMultiplier});
   }
 
   @override
-  void updateIsCompleted(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required bool isCompleted}) {
-    // TODO: implement updateIsCompleted
+  void updateIsCompleted({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required bool isCompleted,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID')
+        .update({'isCompleted': isCompleted});
   }
 
   @override
-  void updateIsImportant(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required bool isImportant}) {
-    // TODO: implement updateIsImportant
+  void updateIsImportant({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required bool isImportant,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID')
+        .update({'isImportant': isImportant});
   }
 
   @override
-  void updateIsOnMyDay(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required bool isOnMyDay}) {
-    // TODO: implement updateIsOnMyDay
+  void updateIsOnMyDay({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required bool isOnMyDay,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID')
+        .update({'isOnMyDay': isOnMyDay});
   }
 
   @override
-  void updateNote(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required String newNote}) {
-    // TODO: implement updateNote
+  void updateNote({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required String newNote,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID')
+        .update({'note': newNote});
   }
 
   @override
@@ -110,71 +145,103 @@ class FirebaseTaskDatabase implements TaskDatabaseProvider {
       {required String groupID,
       required String taskListID,
       required String taskID,
-      required DateTime? remindTime}) {
-    // TODO: implement updateRemindTime
+      required DateTime? remindTime}) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID')
+        .update({'remindTime': remindTime});
   }
 
   @override
-  void updateRepeatFrequency(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required Frequency? repeatFrequency}) {
-    // TODO: implement updateRepeatFrequency
+  void updateRepeatFrequency({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required Frequency? repeatFrequency,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID')
+        .update({'repeatFrequency': repeatFrequency?.name});
   }
 
   @override
-  void addFile(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required List<String> filePath}) {
-    // TODO: implement addFile
+  void addFile({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required List<String> filePath,
+  }) async {
+    for (String file in filePath) {
+      await ref
+          .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID/filePath/')
+          .child('id${file.hashCode}')
+          .set(file);
+    }
   }
 
   @override
-  void removeFile(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required String filePath}) {
-    // TODO: implement removeFile
+  void removeFile({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required String filePath,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID/filePath')
+        .child('id${filePath.hashCode}')
+        .remove();
   }
 
   @override
-  void addStep(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required TaskStep newStep}) {
-    // TODO: implement addStep
-  }
-  @override
-  void deleteStep(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required String stepID}) {
-    // TODO: implement deleteStep
+  void addStep({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required TaskStep newStep,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID/stepList/')
+        .child('id${newStep.id}')
+        .set(newStep.toMap());
   }
 
   @override
-  void updateStepIsCompleted(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required String stepID,
-      required bool isCompleted}) {
-    // TODO: implement updateStepIsCompleted
+  void deleteStep({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required String stepID,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID/stepList/')
+        .child('is$stepID')
+        .remove();
   }
 
   @override
-  void updateStepName(
-      {required String groupID,
-      required String taskListID,
-      required String taskID,
-      required String stepID,
-      required String newName}) {
-    // TODO: implement updateStepName
+  void updateStepIsCompleted({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required String stepID,
+    required bool isCompleted,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID/stepList/')
+        .child('id$stepID')
+        .update({'isCompleted': isCompleted});
+  }
+
+  @override
+  void updateStepName({
+    required String groupID,
+    required String taskListID,
+    required String taskID,
+    required String stepID,
+    required String newName,
+  }) async {
+    await ref
+        .child('id$groupID/taskLists/id$taskListID/tasks/id$taskID/stepList/')
+        .child('id$stepID')
+        .update({'stepName': newName});
   }
 }
